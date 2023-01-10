5f9bfdd7a0dc43ea9a4941b6061f639aaad1fafb
Diego Ramos <diegorbrito9@gmail.com>
2023-01-10 18:44:56 -0300

    feat: criando um 'wrapper' e estilizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import styles from "./styles/app.module.scss";
+
 import { Header } from "./components/Header/Header";
+import { InputContainer } from "./components/InputContainer/InputContainer";
 
 export function App() {
   return (
     <>
       <Header />
+      <main className={styles.mainContainer}>
+        <InputContainer />
+      </main>
     </>
   );
 }
