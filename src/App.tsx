16b6babd2a2be8c1ea37ba79adfd97416543111d
Diego Ramos <diegorbrito9@gmail.com>
2023-01-10 20:10:37 -0300

    feat: centralizando o 'main' container

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./styles/app.module.scss";
 
 import { Header } from "./components/Header/Header";
 import { InputContainer } from "./components/InputContainer/InputContainer";
+import { TaskContainer } from "./components/TaskContainer/TaskContainer";
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
       <Header />
       <main className={styles.mainContainer}>
         <InputContainer />
+        <TaskContainer />
       </main>
     </>
   );
